9a99b4fa6e72e24ea1e55e70ec3eb417f70e425a
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-21 22:16:09 +0200

    Add Role class

---
+++ src/main/java/com/portfolio/easyjob/model/Role.java
@@ -0,0 +1,25 @@
+package com.portfolio.easyjob.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
+
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }
+}
