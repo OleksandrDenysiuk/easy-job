2cb48ad61fad57377395f0937e70fc4d70aeb0ef
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-23 16:37:57 +0200

    Added findByName method

--- src/main/java/com/portfolio/easyjob/repository/RoleRepository.java
+++ src/main/java/com/portfolio/easyjob/repository/RoleRepository.java
@@ -4,4 +4,5 @@ import com.portfolio.easyjob.model.Role;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoleRepository extends CrudRepository<Role, Long> {
+    Role findByName(String name);
 }
