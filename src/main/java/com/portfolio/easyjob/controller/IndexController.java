63e5eb2e1fca760a21c47159b297f63bb0cad1c5
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-23 16:35:30 +0200

    Added index page controller

---
+++ src/main/java/com/portfolio/easyjob/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.portfolio.easyjob.controller;
+
+import com.portfolio.easyjob.model.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String loadPage(@AuthenticationPrincipal User user, Model model){
+        return "index";
+    }
+}
