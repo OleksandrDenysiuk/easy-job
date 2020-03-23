d111cd9401e7156b4d0e2837d15119bcd336b51f
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-23 16:36:12 +0200

    Added post method for register new user

--- src/main/java/com/portfolio/easyjob/controller/RegistrationController.java
+++ src/main/java/com/portfolio/easyjob/controller/RegistrationController.java
@@ -1,12 +1,22 @@
 package com.portfolio.easyjob.controller;
 
 import com.portfolio.easyjob.model.User;
+import com.portfolio.easyjob.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class RegistrationController {
+    private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/registration")
     public String loadPage(Model model){
@@ -14,4 +24,10 @@ public class RegistrationController {
         return "/authentication/registration/form";
     }
 
+
+    @PostMapping("/registration")
+    public String registerUser(@ModelAttribute User user){
+        userService.create(user);
+        return "redirect:/";
+    }
 }
