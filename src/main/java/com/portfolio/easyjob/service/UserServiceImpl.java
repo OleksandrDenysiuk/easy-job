8059d141fbf2c91d09cc2dcbe2ad47cbd30ea7bc
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-23 16:38:48 +0200

    Added implementation for saving user in db

--- src/main/java/com/portfolio/easyjob/service/UserServiceImpl.java
+++ src/main/java/com/portfolio/easyjob/service/UserServiceImpl.java
@@ -1,19 +1,27 @@
 package com.portfolio.easyjob.service;
 
+import com.portfolio.easyjob.model.Role;
 import com.portfolio.easyjob.model.User;
+import com.portfolio.easyjob.repository.RoleRepository;
 import com.portfolio.easyjob.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
@@ -22,7 +30,7 @@ public class UserServiceImpl implements UserService {
 
         User user = userRepository.findByUsername(username);
 
-        if(user == null) throw new UsernameNotFoundException(username);
+        if (user == null) throw new UsernameNotFoundException(username);
 
         return user;
     }
@@ -30,6 +38,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(User user) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return null;
+        }
+
+        Role userRole = roleRepository.findByName("USER");
+        user.setRoles(Arrays.asList(userRole));
+        user.setPassword(new BCryptPasswordEncoder(8).encode(user.getPassword()));
+        userRepository.save(user);
         return null;
     }
 }
