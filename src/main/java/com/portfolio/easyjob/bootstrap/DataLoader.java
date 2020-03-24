7af3339fc8eb0b91d2f29711a66cc94796a09dfe
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-24 10:04:05 +0200

    Added employee and employer

--- src/main/java/com/portfolio/easyjob/bootstrap/DataLoader.java
+++ src/main/java/com/portfolio/easyjob/bootstrap/DataLoader.java
@@ -1,7 +1,9 @@
 package com.portfolio.easyjob.bootstrap;
 
 import com.portfolio.easyjob.domain.Role;
+import com.portfolio.easyjob.domain.User;
 import com.portfolio.easyjob.repository.RoleRepository;
+import com.portfolio.easyjob.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +14,11 @@ import java.util.Set;
 public class DataLoader implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
+    private final UserService userService;
 
-    public DataLoader(RoleRepository roleRepository) {
+    public DataLoader(RoleRepository roleRepository, UserService userService) {
         this.roleRepository = roleRepository;
+        this.userService = userService;
     }
 
     @Override
@@ -32,6 +36,22 @@ public class DataLoader implements CommandLineRunner {
 
     private void loadData(){
         Role userRole = new Role(1L,"USER");
+        Role employeeRole = new Role(2L,"EMPLOYEE");
+        Role employerRole = new Role(3L,"EMPLOYER");
         roleRepository.save(userRole);
+        roleRepository.save(employeeRole);
+        roleRepository.save(employerRole);
+
+        User userEmployee = new User();
+        userEmployee.setUsername("AlexEE");
+        userEmployee.setPassword("1");
+
+        userService.create(userEmployee,"employee");
+
+        User userEmployer = new User();
+        userEmployer.setUsername("AlexER");
+        userEmployer.setPassword("1");
+
+        userService.create(userEmployer,"employer");
     }
 }
