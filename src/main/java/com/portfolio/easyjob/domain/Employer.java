77008517405cfffd2433187af35284e74d8311a6
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-24 10:06:43 +0200

    Added Employer Entity

---
+++ src/main/java/com/portfolio/easyjob/domain/Employer.java
@@ -0,0 +1,44 @@
+package com.portfolio.easyjob.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employer{
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne
+    @MapsId
+    private User user;
+
+    private String nameOfOrganization;
+
+    private String email;
+
+    private int bankAccountNumber;
+
+    private String phoneNumber;
+
+    private String document;
+
+    public Employer(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Employer{" +
+                "id=" + id +
+                ", nameOfOrganization='" + nameOfOrganization + '\'' +
+                '}';
+    }
+}
