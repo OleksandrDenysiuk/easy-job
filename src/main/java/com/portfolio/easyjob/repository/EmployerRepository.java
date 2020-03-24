2d5adff9ab562546f861d0de2d4dd5b15877dc69
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-24 10:08:35 +0200

    Added Employer Repository

---
+++ src/main/java/com/portfolio/easyjob/repository/EmployerRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.easyjob.repository;
+
+import com.portfolio.easyjob.domain.Employer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployerRepository extends CrudRepository<Employer, Long> {
+}
