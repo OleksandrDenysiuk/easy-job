3f90642bbcf1db421606b6ac739c46413d558b3f
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-25 23:43:48 +0200

    Added Message Repository

---
+++ src/main/java/com/portfolio/easyjob/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.portfolio.easyjob.repository;
+
+import com.portfolio.easyjob.domain.Message;
+import com.portfolio.easyjob.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findAllByRecipient(User user);
+}
