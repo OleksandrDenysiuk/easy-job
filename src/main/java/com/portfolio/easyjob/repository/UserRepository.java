94d97e091d7a32be6116d958a1978a552520759e
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-25 23:44:34 +0200

    Added findByRolesEquals method

--- src/main/java/com/portfolio/easyjob/repository/UserRepository.java
+++ src/main/java/com/portfolio/easyjob/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.portfolio.easyjob.repository;
 
+import com.portfolio.easyjob.domain.Role;
 import com.portfolio.easyjob.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String name);
+
+    User findByRolesEquals(Role role);
 }
