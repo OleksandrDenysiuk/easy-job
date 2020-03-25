ee263230f133db6251dc52027b66bfc1359a5aba
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-25 23:37:27 +0200

    Added type of message

--- src/main/java/com/portfolio/easyjob/bootstrap/DataLoader.java
+++ src/main/java/com/portfolio/easyjob/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.portfolio.easyjob.bootstrap;
 
 import com.portfolio.easyjob.domain.Role;
+import com.portfolio.easyjob.domain.TypeOfMessage;
 import com.portfolio.easyjob.domain.User;
 import com.portfolio.easyjob.repository.RoleRepository;
+import com.portfolio.easyjob.repository.TypesOfMessageRepository;
 import com.portfolio.easyjob.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,10 +17,12 @@ public class DataLoader implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
     private final UserService userService;
+    private final TypesOfMessageRepository typesOfMessageRepository;
 
-    public DataLoader(RoleRepository roleRepository, UserService userService) {
+    public DataLoader(RoleRepository roleRepository, UserService userService, TypesOfMessageRepository typesOfMessageRepository) {
         this.roleRepository = roleRepository;
         this.userService = userService;
+        this.typesOfMessageRepository = typesOfMessageRepository;
     }
 
     @Override
@@ -38,9 +42,15 @@ public class DataLoader implements CommandLineRunner {
         Role userRole = new Role(1L,"USER");
         Role employeeRole = new Role(2L,"EMPLOYEE");
         Role employerRole = new Role(3L,"EMPLOYER");
+        Role adminRole = new Role(4L,"ADMIN");
         roleRepository.save(userRole);
         roleRepository.save(employeeRole);
         roleRepository.save(employerRole);
+        roleRepository.save(adminRole);
+
+        TypeOfMessage verifyDocTypeMessage = new TypeOfMessage(1L,"VERIFY_DOC");
+
+        typesOfMessageRepository.save(verifyDocTypeMessage);
 
         User userEmployee = new User();
         userEmployee.setUsername("AlexEE");
@@ -53,5 +63,11 @@ public class DataLoader implements CommandLineRunner {
         userEmployer.setPassword("1");
 
         userService.create(userEmployer,"employer");
+
+        User userAdmin = new User();
+        userAdmin.setUsername("admin");
+        userAdmin.setPassword("1");
+
+        userService.create(userAdmin,"admin");
     }
 }
