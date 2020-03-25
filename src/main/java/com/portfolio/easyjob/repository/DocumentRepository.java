c9c430f0978003eba8b915d37c1cd0aafea052b1
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-25 23:43:35 +0200

    Added Document Repository

---
+++ src/main/java/com/portfolio/easyjob/repository/DocumentRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.easyjob.repository;
+
+import com.portfolio.easyjob.domain.Document;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DocumentRepository extends CrudRepository<Document, Long> {
+}
