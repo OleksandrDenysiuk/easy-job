be52e992d90b7583e17c6bf9c2f7d9db77e420a2
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-29 22:32:38 +0300

    Added method

--- src/main/java/com/portfolio/easyjob/repository/DocumentRepository.java
+++ src/main/java/com/portfolio/easyjob/repository/DocumentRepository.java
@@ -1,7 +1,10 @@
 package com.portfolio.easyjob.repository;
 
 import com.portfolio.easyjob.domain.Document;
+import com.portfolio.easyjob.domain.TypeOfDocument;
+import com.portfolio.easyjob.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DocumentRepository extends CrudRepository<Document, Long> {
+    Document findByUserAndType(User user, TypeOfDocument typeOfDocument);
 }
