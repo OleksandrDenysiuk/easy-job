aa3d3da174ac44261053644aacacaa45b2290864
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-29 22:32:48 +0300

    Added method

--- src/main/java/com/portfolio/easyjob/repository/MessageRepository.java
+++ src/main/java/com/portfolio/easyjob/repository/MessageRepository.java
@@ -1,6 +1,7 @@
 package com.portfolio.easyjob.repository;
 
 import com.portfolio.easyjob.domain.Message;
+import com.portfolio.easyjob.domain.TypeOfMessage;
 import com.portfolio.easyjob.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface MessageRepository extends CrudRepository<Message, Long> {
     List<Message> findAllByRecipient(User user);
+
+    Message findByRecipientAndType(User user, TypeOfMessage type);
 }
