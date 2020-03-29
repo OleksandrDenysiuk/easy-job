914cc6540349e1daabdc9a2e9dfd2fb8b3d91243
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-29 22:28:46 +0300

    Added TypeOfDocument, StatusOfDocument, TypeOfMessage

--- src/main/java/com/portfolio/easyjob/bootstrap/DataLoader.java
+++ src/main/java/com/portfolio/easyjob/bootstrap/DataLoader.java
@@ -1,9 +1,9 @@
 package com.portfolio.easyjob.bootstrap;
 
-import com.portfolio.easyjob.domain.Role;
-import com.portfolio.easyjob.domain.TypeOfMessage;
-import com.portfolio.easyjob.domain.User;
+import com.portfolio.easyjob.domain.*;
 import com.portfolio.easyjob.repository.RoleRepository;
+import com.portfolio.easyjob.repository.StatusOfDocumentRepository;
+import com.portfolio.easyjob.repository.TypeOfDocumentRepository;
 import com.portfolio.easyjob.repository.TypesOfMessageRepository;
 import com.portfolio.easyjob.service.UserService;
 import org.springframework.boot.CommandLineRunner;
@@ -18,11 +18,19 @@ public class DataLoader implements CommandLineRunner {
     private final RoleRepository roleRepository;
     private final UserService userService;
     private final TypesOfMessageRepository typesOfMessageRepository;
+    private final TypeOfDocumentRepository typeOfDocumentRepository;
+    private final StatusOfDocumentRepository statusOfDocumentRepository;
 
-    public DataLoader(RoleRepository roleRepository, UserService userService, TypesOfMessageRepository typesOfMessageRepository) {
+    public DataLoader(RoleRepository roleRepository,
+                      UserService userService,
+                      TypesOfMessageRepository typesOfMessageRepository,
+                      TypeOfDocumentRepository typeOfDocumentRepository,
+                      StatusOfDocumentRepository statusOfDocumentRepository) {
         this.roleRepository = roleRepository;
         this.userService = userService;
         this.typesOfMessageRepository = typesOfMessageRepository;
+        this.typeOfDocumentRepository = typeOfDocumentRepository;
+        this.statusOfDocumentRepository = statusOfDocumentRepository;
     }
 
     @Override
@@ -40,17 +48,41 @@ public class DataLoader implements CommandLineRunner {
 
     private void loadData(){
         Role userRole = new Role(1L,"USER");
-        Role employeeRole = new Role(2L,"EMPLOYEE");
-        Role employerRole = new Role(3L,"EMPLOYER");
-        Role adminRole = new Role(4L,"ADMIN");
         roleRepository.save(userRole);
+
+        Role employeeRole = new Role(2L,"EMPLOYEE");
         roleRepository.save(employeeRole);
+
+        Role employerRole = new Role(3L,"EMPLOYER");
         roleRepository.save(employerRole);
+
+        Role adminRole = new Role(4L,"ADMIN");
         roleRepository.save(adminRole);
 
         TypeOfMessage verifyDocTypeMessage = new TypeOfMessage(1L,"VERIFY_DOC");
-
         typesOfMessageRepository.save(verifyDocTypeMessage);
+
+        TypeOfMessage resultVerifyingTypeMessage = new TypeOfMessage(2L,"VERIFY_RESULT");
+        typesOfMessageRepository.save(resultVerifyingTypeMessage);
+
+        TypeOfDocument passportTypeDocument = new TypeOfDocument(1L, "PASSPORT");
+        typeOfDocumentRepository.save(passportTypeDocument);
+
+        TypeOfDocument photoUserTypeDocument = new TypeOfDocument(2L, "PHOTO_USER");
+        typeOfDocumentRepository.save(photoUserTypeDocument);
+
+        TypeOfDocument legitimationTypeDocument = new TypeOfDocument(3L, "LEGITIMATION");
+        typeOfDocumentRepository.save(legitimationTypeDocument);
+
+        StatusOfDocument verifiedStatusOfDocument = new StatusOfDocument(1L, "VERIFIED");
+        statusOfDocumentRepository.save(verifiedStatusOfDocument);
+
+        StatusOfDocument inProgressStatusOfDocument = new StatusOfDocument(2L, "IN_PROGRESS");
+        statusOfDocumentRepository.save(inProgressStatusOfDocument);
+
+        StatusOfDocument notVerifiedStatusOfDocument = new StatusOfDocument(3L, "NOT_VERIFIED");
+        statusOfDocumentRepository.save(notVerifiedStatusOfDocument);
+
 
         User userEmployee = new User();
         userEmployee.setUsername("AlexEE");
