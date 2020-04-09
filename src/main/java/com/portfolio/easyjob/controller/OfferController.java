ad1cee3ab5bc9055f85db45f405c13210a390c53
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-04-09 23:17:35 +0300

    Added model object in get controller. Added post controller

--- src/main/java/com/portfolio/easyjob/controller/OfferController.java
+++ src/main/java/com/portfolio/easyjob/controller/OfferController.java
@@ -1,11 +1,13 @@
 package com.portfolio.easyjob.controller;
 
 
+import com.portfolio.easyjob.domain.Offer;
 import com.portfolio.easyjob.domain.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,14 +19,16 @@ public class OfferController {
     public String loadForm(@AuthenticationPrincipal User user,
                            Model model){
         model.addAttribute("user", user);
+        model.addAttribute("offer", new Offer());
         return "offer/form";
     }
 
     @PostMapping("/create")
-    public String createOffer(){
+    public String createOffer(@AuthenticationPrincipal User user,
+                              @ModelAttribute Offer offer){
 
+        System.out.println(offer.toString());
 
-
-        return "index";
+        return "redirect:/";
     }
 }
