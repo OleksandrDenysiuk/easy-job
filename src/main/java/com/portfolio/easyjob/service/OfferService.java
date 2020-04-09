c5d73a009fa489a2779d60336511c58cdc496076
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-04-09 23:18:20 +0300

    Added create method

--- src/main/java/com/portfolio/easyjob/service/OfferService.java
+++ src/main/java/com/portfolio/easyjob/service/OfferService.java
@@ -1,4 +1,7 @@
 package com.portfolio.easyjob.service;
 
+import com.portfolio.easyjob.domain.Offer;
+
 public interface OfferService {
+    Offer create(Offer offer);
 }
--- src/main/java/com/portfolio/easyjob/service/OfferServiceImpl.java
+++ src/main/java/com/portfolio/easyjob/service/OfferServiceImpl.java
@@ -1,7 +1,13 @@
 package com.portfolio.easyjob.service;
 
+import com.portfolio.easyjob.domain.Offer;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OfferServiceImpl implements OfferService{
+
+    @Override
+    public Offer create(Offer offer) {
+        return null;
+    }
 }
