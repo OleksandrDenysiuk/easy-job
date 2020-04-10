b124f3ae5c80d9563c1a59f0bb7a1bc880d499a2
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-04-10 23:13:15 +0300

    Added OfferRepository

---
+++ src/main/java/com/portfolio/easyjob/repository/OfferRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.easyjob.repository;
+
+import com.portfolio.easyjob.domain.Offer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OfferRepository extends CrudRepository<Offer, Long> {
+}
